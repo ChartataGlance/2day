398a460d7ebefcb470fe7bdefea31e39a61365c4
chartataglance <mariadass@msn.com>
2022-02-23 12:17:19 +0000

    hi

---
+++ ganalatic.js
@@ -0,0 +1,3 @@
+
+
+<script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='MMERGE3';ftypes[3]='dropdown';fnames[4]='MMERGE4';ftypes[4]='dropdown';fnames[5]='MMERGE5';ftypes[5]='dropdown';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
